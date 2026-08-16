a188e2adefc3d4ee01462ae6e12a69591e2ae2fc
Neil Enns <neile@live.com>
2026-08-16 06:42:30 -0700

    Tweak things

--- mobiflight/post-create.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-# All paths need to be made absolute since it runs in the context of a devcontainer.
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-npm install
